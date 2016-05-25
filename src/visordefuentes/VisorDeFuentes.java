91c015b213e7eae5aab19063626db00a330e6888
Cab Martinez Rodolfo <recm_95@hotmail.com>
2016-05-25 11:17:53 -0500

    Diseño terminado

---
+++ src/visordefuentes/VisorDeFuentes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visordefuentes;
+
+import gui.Visual;
+
+/**
+ *
+ * @author RodolfoECM
+ */
+public class VisorDeFuentes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Visual bebe=new Visual();
+      bebe.setLocationRelativeTo(null);
+      bebe.setVisible(true);
+    }
+    
+}
